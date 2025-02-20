65df06f7ef3d90d7f8ed37e3491bbaa4fea2e461
janoxxx <janoxxx@ttsk.ngo>
2025-02-20 22:20:15 +0100

    login and 404 page

--- Integrator.Frontend/Program.cs
+++ Integrator.Frontend/Program.cs
@@ -20,6 +20,8 @@ try
 
     var app = builder.Build();
 
+    app.UseStatusCodePagesWithReExecute("/errors/{0}");
+    
     // Configure the HTTP request pipeline.
     if (!app.Environment.IsDevelopment())
     {
