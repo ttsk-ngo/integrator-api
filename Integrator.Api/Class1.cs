0e6abbec8e9f22ef9f1ed6f9e3857cf893916991
janoxxx <janoxxx@ttsk.ngo>
2025-02-20 18:47:45 +0100

    separate applications for API and frontend

--- Integrator.Api/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Integrator.Api;
-
-public class Class1
-{
-}
---
+++ Integrator.Api/HostingExtensions/MiddlewareConfigurator.cs
@@ -0,0 +1,16 @@
+﻿using Integrator.Frontend.Middlewares;
+
+namespace Integrator.Frontend.HostingExtensions;
+
+internal static class MiddlewareConfigurator
+{
+    internal static void AddScopedMiddlewares(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<GlobalExceptionMiddleware>();
+    }
+
+    internal static void UseMiddlewares(this WebApplication app)
+    {
+        app.UseMiddleware<GlobalExceptionMiddleware>();
+    }
+}
