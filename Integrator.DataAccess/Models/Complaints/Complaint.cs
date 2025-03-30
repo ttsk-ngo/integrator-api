c173e9403845108b84a5e43b3c0a2346f42f8eeb
janoxxx <janoxxx@ttsk.ngo>
2025-03-30 21:13:58 +0200

    feat: Add simple complaint DB model
    Complaint model with initial DB migration

---
+++ Integrator.DataAccess/Models/Complaints/Complaint.cs
@@ -0,0 +1,7 @@
+﻿namespace Integrator.DataAccess.Models.Complaints;
+
+public class Complaint : BaseModel
+{
+    public string ComplaintNumber { get; set; } // Easy to write complaint number
+    public string ComplaintDescription { get; set; }
+}
