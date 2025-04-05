5b9da201750696c4ce53f34eb1227029724f61f1
janoxxx <janoxxx@ttsk.ngo>
2025-04-05 20:42:30 +0200

    feat: Add abstraction of database repositories

---
+++ Integrator.DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Integrator.DataAccess.Repositories;
+
+public interface IRepository<TModel>
+{
+    public TModel GetAll();
+    public TModel GetById(string id);
+    public TModel Add(TModel model);
+    public TModel Update(TModel model);
+    public TModel Remove(string id);
+}
