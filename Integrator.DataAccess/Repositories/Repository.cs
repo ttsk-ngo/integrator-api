3ded0fb39282d7abe1049372fc11906bbb44ce39
janoxxx <janoxxx@ttsk.ngo>
2025-04-05 20:42:30 +0200

    feat: Add abstraction of database repositories

---
+++ Integrator.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,29 @@
+﻿namespace Integrator.DataAccess.Repositories;
+
+public class Repository<TModel> : IRepository<TModel>
+{
+    public TModel GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public TModel GetById(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TModel Add(TModel model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TModel Update(TModel model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TModel Remove(string id)
+    {
+        throw new NotImplementedException();
+    }
+}
