fd7dbad5db98e4c911ae31067e78c04eef8f3151
janoxxx <janoxxx@ttsk.ngo>
2025-04-27 19:55:14 +0200

    refactor: Fix build warnings

--- Integrator.Frontend/WebInterface/Pages/Login/LoginModel.cs
+++ Integrator.Frontend/WebInterface/Pages/Login/LoginModel.cs
@@ -6,8 +6,9 @@ public class LoginModel
 {
     [Required(ErrorMessage = "Email is required")]
     [EmailAddress(ErrorMessage = "Invalid email address")]
-    public string Email { get; set; }
+    public string Email { get; set; } = null!;
+
     [Required(ErrorMessage = "Password is required")]
-    public string Password { get; set; }
+    public string Password { get; set; } = null!;
     public bool RememberMe { get; set; }
 }
